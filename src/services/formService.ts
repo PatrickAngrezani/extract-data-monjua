9313567e6cc50f9893bae408e224bf020ac5236a
PatrickAngrezani <pangrezani@gmail.com>
2025-02-04 16:40:07 -0300

    feat: format datas

--- src/services/formService.ts
+++ src/services/formService.ts
@@ -1,39 +1,59 @@
 export interface FormResponse {
-    data: Date,
-    ticketID: string,
-    filial: string,
-    perguntas: string[];
-    respostas: string[];
+  data: Date;
+  ticketID: string;
+  filial: string;
+  perguntas: string[];
+  respostas: string[];
+}
+
+export const processFormData = (rawData): FormResponse => {
+  let formattedDate: Date;
+  let ticketID: string;
+  let branchName;
+  let questions;
+
+  for (const data of rawData) {
+    const creationDate = new Date(data.payload.createdTime);
+    formattedDate = new Date(creationDate.toLocaleDateString("pt-BR"));
+
+    ticketID = data.payload.ticketNumber;
+
+    //capture origin
+    branchName = captureBranchName(data);
+    questions = captureQuestions(data);
+  }
+
+  questions.sort((a, b) => a.pergunta.localeCompare(b.pergunta));
+
+  return {
+    data: formattedDate,
+    ticketID,
+    filial: branchName,
+    perguntas: questions.map((q) => q.pergunta),
+    respostas: questions.map((q) => q.resposta),
+  };
 };
 
-export const processFormData = (rawData): FormResponse => {   
-    let formattedDate: Date;
-    let ticketID: string;
-    let branchName;
-    let questions;
+function captureBranchName(data: any): string {
+  const branchName =
+    Object.entries(data.payload.customFields).find(
+      ([question]) => question === "Selecione sua filial"
+    )[1] || null;
 
-    for (const data of rawData) {
-        const creationDate = new Date(data.payload.createdTime);
-        formattedDate = new Date(creationDate.toLocaleDateString('pt-BR'));
+  return String(branchName);
+}
 
-        ticketID = data.payload.ticketNumber;
+function captureQuestions(data: any): {} {
+  const questions = Object.entries(data.payload.customFields)
+    .filter(
+      ([question, response]) =>
+        response !== null &&
+        question !== "Nome para contato" &&
+        question !== "Selecione sua filial" &&
+        question !== "Teste" &&
+        question !== "Serviço"
+    )
+    .map(([pergunta, resposta]) => ({ pergunta, resposta }));
 
-        //capture origin
-        branchName = Object.entries(data.payload.customFields)
-        .find(([question]) => question === 'Selecione sua filial')[1] || null;
-        
-        questions = Object.entries(data.payload.customFields)
-        .filter(([question, response]) => response !== null && question !== 'Nome para contato' && question !== 'Selecione sua filial' && question !== 'Teste' && question !== 'Serviço' )
-        .map(([pergunta, resposta]) => ({ pergunta, resposta }));
-    }
-
-    questions.sort((a, b) => a.pergunta.localeCompare(b.pergunta));    
-
-    return { 
-        data: formattedDate, 
-        ticketID, 
-        filial: branchName, 
-        perguntas: questions.map(q => q.pergunta), 
-        respostas: questions.map(q => q.resposta) 
-    };
-};
+  return questions;
+}
