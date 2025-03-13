fbeb621ab5cbb25e3acc46a3f6697637d413e180
PatrickAngrezani <pangrezani@gmail.com>
2025-03-13 14:05:29 -0300

    feat: access compliance tickets and extract values

--- src/controllers/ticketsController.ts
+++ src/controllers/ticketsController.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
 import { getAccessToken, refreshToken } from "../services/authService.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import * as dotenv from "dotenv";
 
-const departmentId = "87418000000718100";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: `${__dirname}/../../.env` });
+
+const departmentId = process.env.DEPARTMENT_ID;
+
+let lastTicketReviewed: number = 0;
+let complianceTicketsArray: Record<string, any>[] = [];
 
 export const retrieveTickets = async () => {
   try {
@@ -14,16 +25,72 @@ export const retrieveTickets = async () => {
       },
     });
 
-    response.data.data.forEach((item, index) => {
-        console.log({dis: item.departmentId});
-        console.log(`Item ${index + 1}:`, item);
-        
-      if (item.departmentId == 87418000000718100) {
-        console.log(`Item ${index + 1}:`, item);
-      }
-    });
+    // promisse.all + map (mapeia resultados e aguarda todas requisições desejadas)
+    await Promise.all(
+      response.data.data
+        .filter(
+          (item: { departmentId: string }) => item.departmentId == departmentId
+        ) // filtra departamento correto
+        .map(async (item: { id: string }) => {
+          try {
+            const complianceTickets = await axios.get(
+              `https://desk.zoho.com/api/v1/tickets/${item.id}?include=departments`,
+              {
+                headers: {
+                  Authorization: `Zoho-oauthtoken ${accessToken}`,
+                  orgId: Number(814785537),
+                },
+              }
+            );
 
-    return response.data;
+            // filtra tickets do compliance
+            if (
+              complianceTickets.data.customFields["Serviço"] ===
+              "Auditoria Compliance"
+            ) {
+              // filtra questionário
+              // monta array de pares valores [key, value]
+              const filteredData = Object.entries(
+                complianceTickets.data.customFields
+              )
+                .filter(
+                  ([question, response]) =>
+                    response !== null &&
+                    ![
+                      "Nome para contato",
+                      "Selecione sua filial",
+                      "Teste",
+                      "Serviço",
+                    ].includes(question)
+                )
+                // transforma o array em objeto | acc obj vazio inicialmente {}
+                .reduce((acc, [key, value]) => {
+                  acc[key] = value;
+                  return acc;
+                }, {} as Record<string, any>);
+
+              // adiciona os dados filtrados ao array de tickets processados
+              complianceTicketsArray.push({
+                ticketNumber: Number(complianceTickets.data["ticketNumber"]),
+                data: filteredData,
+              });
+
+              // atualiza lastTicketReviewed para o maior ticketNumber encontrado
+              lastTicketReviewed = Math.max(
+                lastTicketReviewed,
+                Number(complianceTickets.data["ticketNumber"])
+              );
+            }
+          } catch (error) {
+            console.error(
+              `Erro ao processar o ticket ${item.id}:`,
+              error.message
+            );
+          }
+        })
+    );
+
+    return complianceTicketsArray;
   } catch (error: any) {
     console.error("Error retrieving tickets:", error.response?.data);
 
